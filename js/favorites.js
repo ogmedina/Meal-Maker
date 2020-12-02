b70c9b6b14cb4fa31c28867de57b775961acc2c1
Matthew Marchand <marchandmr@icloud.com>
2020-12-01 20:08:15 -0600

    favorites page is working

--- js/favorites.js
+++ js/favorites.js
@@ -1,20 +1,56 @@
 var deleteButtonEl = document.getElementById('delete-button');
 
+
+var foodArray = JSON.parse(localStorage.getItem("food"));
+console.log(foodArray);
 function food() {
 
-  var newCard = $("<div></div>")
-  newCard.attr('class', 'card')
+  for (var i = 0; i < foodArray.length; i++) {
+    var newRow = $("<div></div>")
+    newRow.attr('class', 'rowContainer')
+    newRow.attr('data-index', i)
+    var cardHeader = $("<div>" + "<h4>" + foodArray[i].name + "</h4>" + "</div>")    // title of drink will be stored here
+    cardHeader.attr('class', 'card-divider')
+    newRow.append(cardHeader)
+    //add the image
+    var newImg = $("<a href = " + foodArray[i].link + " target='_blank'" + "><img src =" + foodArray[i].img + " width=\"180\" height=\"120\"></a>");
+    // newImg.attr('height', '120')
+    // newImg.attr('width', '180')
+    newRow.append(newImg)
+    var ingredients = $("<li></ul>") // ingredients to make drinks will be stored here
+    ingredients.attr('class', 'Ingredients')
+    newRow.append(ingredients)
+    for (var j = 0; j < foodArray[i].ingredients.length; j++) {
+      ingredientsItem = $("<ul></ul>")
+      ingredientsItem.attr('class', 'ingredientsItem')
+      ingredientsItem.text(foodArray[i].ingredients[j])
+      ingredients.append(ingredientsItem)
+    }
+    //directions
 
-  var cardHeader = $("<div>" + "<h4>" + "Food Title" + "</h4>" + "</div>")
-  cardHeader.attr('class', 'card-divider')
-  var img = $("<img>")
-  img.attr('src', "https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277__340.jpg")
-  img.attr('height', '120')
-  img.attr('width', '180')
-  newCard.append(cardHeader)
-  newCard.append(img);
-  $("#food").append(newCard);
+    // button to remove from favorites
+    var removeButton = $("<button></button>")
+    removeButton.attr('class', 'button delete-button')
+    removeButton.text("Delete")
+    removeButton.attr('data-index', i)
+    newRow.append(removeButton)
+    $("#food").append(newRow)
+    $("#food").append('<hr>')
+  }
+
+
 }
+
+$("#food").on('click', function (event) {
+  event.preventDefault();
+  if (event.target.matches('.delete-button')) {
+    removeFood(event.target.dataset.index);
+  }
+})
+
+
+
+
 
 
 
@@ -65,19 +101,30 @@ function drink() {
 $("#drink").on('click', function (event) {
   event.preventDefault();
   if (event.target.matches('.delete-button')) {
-    remove(event.target.dataset.index);
+    removeDrink(event.target.dataset.index);
   }
 })
 
 
 
 
-function remove(index) {
+function removeDrink(index) {
   console.log(index);
   console.log(drinksArray)
   drinksArray.splice(index, 1);
   console.log(drinksArray);
   localStorage.setItem('drinks', JSON.stringify(drinksArray))
+  refreshPage()
+  //localStorage.removeItem('drinks'.[index]);
+
+}
+
+function removeFood(index) {
+  console.log(index);
+  console.log(foodArray)
+  foodArray.splice(index, 1);
+  console.log(foodArray);
+  localStorage.setItem('food', JSON.stringify(foodArray))
   refreshPage()
   //localStorage.removeItem('drinks'.[index]);
 
